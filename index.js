5d27f0c51086c2b3e5f38d42eb28175e84adbd99
Fcooperation <thefcooperation@gmail.com>
2025-06-19 13:48:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000; // Render uses process.env.PORT
-
-app.get('/', (req, res) => {
-  res.send('🚀 Hello from Fweb via Render + GitHub!');
-});
-
-app.listen(port, () => {
-  console.log(`✅ Server running on port ${port}`);
-});
+// index.js
+require('./fcrawler');
