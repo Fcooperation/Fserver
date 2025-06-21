75f17fcdff91c139043daef32d546bee871ed1a9
Fcooperation <thefcooperation@gmail.com>
2025-06-21 08:56:34 +0200

    Update fcrawler.js

--- fcrawler.js
+++ fcrawler.js
@@ -1,14 +1,11 @@
-// fcrawler.js
-const chromium = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer');
 
 (async () => {
   console.log("Launching headless browser...");
 
   const browser = await puppeteer.launch({
-    args: chromium.args,
-    executablePath: await chromium.executablePath,
-    headless: chromium.headless,
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
 
   const page = await browser.newPage();
