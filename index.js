044fd55c7c7f72e6fd3683e8efc684ddf02b0310
Fcooperation <thefcooperation@gmail.com>
2025-06-29 23:59:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/api/search', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}/`);
-  const startUrl = 'https://https://file-examples.com/index.php/sample-documents-download/sample-pdf-download//';
+  const startUrl = 'https://https://file-examples.com/';
   console.log(`📄 Crawling: ${startUrl}`);
   crawlSite(startUrl);
 });
